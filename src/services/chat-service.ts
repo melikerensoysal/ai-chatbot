9fd2301fd2a76635b2970eef2aaec53c5d3fd601
melikerensoysal <melikerensoysal@gmail.com>
2026-01-06 11:29:10 +0300

    last update

--- src/services/chat-service.ts
+++ src/services/chat-service.ts
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI, type Content } from "@google/generative-ai";
-const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+const API_KEY = import.meta.env.GEMINI_API_KEY;
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({
